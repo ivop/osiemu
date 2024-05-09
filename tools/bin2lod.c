91d42586774c7bf27a5c42bad6c4d8be09253bca
Ivo van Poorten <ivopvp@gmail.com>
2024-05-10 00:50:57 +0200

    Add bin2lod tool so we can easily write code with mads and convert the Atari binary output format as that supports multiple blocks and a run address by default.

---
+++ tools/bin2lod.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+static int get16(void) {
+    int a = getchar();
+    return a | (getchar() << 8);
+}
+
+int main(int argc, char **argv) {
+    int start = get16(), end;
+
+    if (start != 0xffff) {
+        fprintf(stderr, "format not recognized\n");
+        return 1;
+    }
+
+    while (1) {
+        if (start == 0xffff) start = get16();
+        end = get16();
+
+        fprintf(stderr, "block: $%04x - $%04x\n", start, end);
+
+        if (start == 0x02e0 && end == 0x02e1) {
+            start = get16();
+            fprintf(stderr, "RUN: $%04x\n", start);
+            printf(".%04XG%c", start, 13);
+            break;
+        }
+
+        printf(".%04X%c/", start, 13);
+        for (; start <= end; start++)
+            printf("%02X%c", getchar(), 13);
+
+        start = get16();
+        if (start < 0) break;
+    }
+}
---
+++ tools/test.s
@@ -0,0 +1,44 @@
+; Mad-Assembler (mads) format
+; Exports Atari binary format, each block is:
+;   FFFF        optional marker, except for first block
+;   LLHH        HHLL start address
+;   LLHH        end address
+;
+; Special block: 02E0-02E1 contains RUN address
+;
+; Assemble with: mads -o:test.bin test.s
+; Convert with: bin2lod < test.bin > test.lod
+
+SCREEN = $d000
+
+    org $0200
+
+; clear screen
+
+start:
+    ldy #0
+    lda #' '
+@:
+    .rept 8
+        sta SCREEN+#*256,y
+    .endr
+    iny
+    bne @-
+
+; Display message
+
+    ldy #message_end-message-1
+@:
+    lda message,y
+    sta SCREEN,y
+    dey
+    bpl @-
+
+    jmp *
+
+message:
+    .byte 'Hello, world!'
+message_end:
+
+    run start
+

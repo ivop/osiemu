60fa901bc17b9d0827cd78bcfaec0f15a6705780
Ivo van Poorten <ivopvp@gmail.com>
2024-05-20 00:52:21 +0200

    Reading bits from bitstream, beginning of ACIA state machine.

--- floppy.c
+++ floppy.c
@@ -52,6 +52,10 @@ struct drive {
     off_t offset;
     char *map;
 
+    unsigned int pos;
+    uint8_t bit;
+    uint8_t curbyte;
+
     unsigned int curtrk;
     bool ready;
     bool r_w;
@@ -81,7 +85,7 @@ static bool hole;
 
 #define DATA_DIRECTION_ACCESS   0x04        // bit 2, 0 active(!)
 
-// PORTA input bits
+// PORTA input bits  (PA6 can be output)
 
 #define DRIVE0_NOT_READY_MASK   0x01        // 0 = drive0 reads, 1 = not ready
 #define HEAD_NOT_TRACK0_MASK    0x02        // 0 = above, 1 = not above
@@ -97,14 +101,28 @@ static bool hole;
 #define DIRECTION_MASK          0x04        // 0 = to track 39, 1 = to track 0
 #define MOVE_HEAD_MASK          0x08        // 1->0 move, 1 = steady
 #define FAULT_RESET_MASK        0x10        // 0 = reset, 1 = normal
-#define DRIVE_ENABLE_MASK       0x20        // 0 = drive1, 1 = drive0
+#define DRIVE_ENABLE_MASK       0x20        // 0 = drive off, 1 = drive on
 #define LOW_CURRENT_MASK        0x40        // 0 = high?, 1 = low
-#define HEAD_ON_DISK_MASK       0x80        // 0 = head on disk, 1 = lift head
+#define HEAD_NOT_ON_DISK_MASK   0x80        // 0 = head on disk, 1 = lift head
 
 #define setbit(v,msk)   (v) |= (msk)
 #define clrbit(v,msk)   (v) &= ~(msk)
 #define getbit(v,msk)   (v & (msk))
 #define cpybit(v,msk,b) { if (b) setbit(v,msk); else clrbit(v,msk); }
+
+// ----------------------------------------------------------------------------
+
+// Framing state-machine
+
+enum acia_state_e {
+    STATE_WAIT_FOR_STARTBIT,
+    STATE_COLLECT_DATABITS,
+    STATE_READ_PARITY,
+    STATE_READ_STOPBIT1,
+    STATE_READ_STOPBIT2
+};
+
+enum acia_state_e acia_state = STATE_WAIT_FOR_STARTBIT;
 
 // ----------------------------------------------------------------------------
 
@@ -140,6 +158,7 @@ static bool login_drive(struct drive *d) {
 
     d->offset = fgetc(d->f) * 256;
     d->curtrk = 7;                  // just somewhere not track 0
+    d->pos = d->bit = 0;
     d->ready = true;
     d->r_w = true;
 
@@ -368,8 +387,26 @@ void floppy_acia_write(uint16_t address, uint8_t value) {
 
 // ----------------------------------------------------------------------------
 
+static bool get_bit(struct drive *d) {
+    if (!d->bit) {
+        d->curbyte = d->map[d->offset + d->curtrk*trksize + d->pos];
+        d->bit = 0x80;
+        d->pos++;
+    }
+
+    bool x = d->curbyte & d->bit;
+    d->bit >>= 1;
+    return x;
+}
+
+// ----------------------------------------------------------------------------
+
 void floppy_tick(double ticks) {
     if (!floppy_enable) {
+        return;
+    }
+
+    if (!(pia.portb.output_value & DRIVE_ENABLE_MASK)) {
         return;
     }
 
@@ -389,7 +426,7 @@ void floppy_tick(double ticks) {
     bits_counter++;
     if (bits_counter >= bits_per_revolution) {
         bits_counter = 0;
-//        drives[curdrive].pos = 0;     // reset position in curtrk of curdrive
+        drives[curdrive].pos = drives[curdrive].bit = 0;   // reset to start
     }
 
     if (bits_counter < bits_per_hole) {
@@ -403,7 +440,42 @@ void floppy_tick(double ticks) {
         return;
     }
 
+    if (pia.portb.output_value & HEAD_NOT_ON_DISK_MASK) {
+        return;
+    }
+
     // collect serial framed bits and pass on to ACIA
+
+    bool bit = get_bit(&drives[curdrive]);
+
+    //printf("floppy: bit: %x\n", bit);
+
+    switch (acia_state) {
+    case STATE_WAIT_FOR_STARTBIT:
+        if (bit) break;
+        acia_state = STATE_COLLECT_DATABITS;
+        // set some value to 8 or whatever was selected and count down later
+        break;
+    case STATE_COLLECT_DATABITS:
+        // if all bits are collected, acia_state = STATE_READ_PARITY
+        // if parity, acia_state = STATE_READ_PARITy
+        // else acia_state = STATE_READ_STOPBIT1
+        break;
+    case STATE_READ_PARITY:
+        // if (bit != calculated_parity) set PE
+        acia_state = STATE_READ_STOPBIT1;
+        break;
+    case STATE_READ_STOPBIT1:
+        if (!bit) break;        // set framing error
+        acia_state = STATE_WAIT_FOR_STARTBIT;   // or STOPBIT2
+        // if last framing bit, copy byte to RDR and set RDRF
+        break;
+    case STATE_READ_STOPBIT2:
+        if (!bit) break;        // set framing error
+        acia_state = STATE_WAIT_FOR_STARTBIT;
+        // copy byte to RDR and set RDRF
+        break;
+    }
 }
 
 // ----------------------------------------------------------------------------

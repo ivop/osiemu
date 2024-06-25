f01c97fd4f9beb776625c6bc3bb4755dde415287
Ivo van Poorten <ivopvp@gmail.com>
2024-06-25 20:15:40 +0200

    All command line options, except those that specify files.

--- launcher/src/mainwindow.cpp
+++ launcher/src/mainwindow.cpp
@@ -23,14 +23,16 @@ void MainWindow::on_button_launch_clicked()
 
     this->hide();
 
+    // Program
+
     QString tmp = "osiemu";
 
     tmp = ui->line_program->text();
     if (!tmp.isEmpty()) program = tmp;
 
-    auto video_mode = ui->combo_video_mode->currentIndex();
+    // Video
 
-    if (!video_mode) {
+    if (!ui->combo_video_mode->currentIndex()) {
         arguments.append("--disable-video");
     } else {
         arguments.append("--video-mode");
@@ -81,10 +83,97 @@ void MainWindow::on_button_launch_clicked()
     if (ui->check_scanlines->checkState() == Qt::Checked) {
         arguments.append("--scanlines");
     }
-
     if (ui->check_smooth_video->checkState() == Qt::Checked) {
         arguments.append("--smooth-video");
     }
+
+    // Keyboard
+
+    if (ui->combo_keyboard->currentIndex() == 0) {
+        arguments.append("--invert-keyboard");
+    }
+    if (ui->combo_cooked_raw->currentIndex() == 1) {
+        arguments.append("--raw-keyboard");
+    }
+    if (ui->check_ascii_keyboard->checkState() == Qt::Checked) {
+        arguments.append("--ascii-keyboard");
+    }
+
+    // Joysticks
+
+    if (ui->check_joystick_1->checkState() == Qt::Checked) {
+        arguments.append("--joystick1");
+        arguments.append(ui->spin_joystick_1->text());
+    }
+    if (ui->check_joystick_2->checkState() == Qt::Checked) {
+        arguments.append("--joystick2");
+        arguments.append(ui->spin_joystick_2->text());
+    }
+
+    // Sound
+
+    arguments.append("--sound-mode");
+
+    switch (ui->combo_sound_mode->currentIndex()) {
+    default:
+        arguments.append("none");
+        break;
+    case 1:
+        arguments.append("542b");
+        break;
+    case 2:
+        arguments.append("600");
+        break;
+    }
+
+    arguments.append("--sound-bufsize");
+    arguments.append(ui->spin_sound_bufsize->text());
+
+    // Clocks
+
+    arguments.append("--cpu-speed");
+
+    switch (ui->combo_cpu_speed->currentIndex()) {
+    default:
+        arguments.append("c1p");
+        break;
+    case 1:
+        arguments.append("510c-slow");
+        break;
+    case 2:
+        arguments.append("510c-fast");
+        break;
+    case 3:
+        arguments.append("uk101");
+        break;
+    case 4:
+        arguments.append("c2p");
+        break;
+    }
+
+    if (ui->check_warp_speed->checkState() == Qt::Checked) {
+        arguments.append("--warp-speed");
+    }
+
+    // Tape
+
+    unsigned int baseclock;
+    switch (ui->combo_tape_baseclock->currentIndex()) {
+    default:    baseclock =   4800; break;
+    case 1:     baseclock =   9600; break;
+    case 2:     baseclock =  19200; break;
+    case 3:     baseclock =  38400; break;
+    case 4:     baseclock =  76800; break;
+    case 5:     baseclock = 153600; break;
+    }
+    arguments.append("--tape-baseclock");
+    arguments.append(QString::number(baseclock));
+
+    arguments.append("--tape-location");
+    arguments.append(ui->combo_tape_location->currentText());
+
+    // Files
+    // TODO
 
     ConsoleWindow *console = new ConsoleWindow(this, program, arguments);
     console->exec();

4cfb99a45856da9bcf560f1f4bb89eec2eff4775
Ivo van Poorten <ivopvp@gmail.com>
2024-10-18 06:27:24 +0200

    launcher: make sure current directory is where the executable is. this is helpful for macOS and doesn't change Linux and Windows behaviour.

--- launcher/src/main.cpp
+++ launcher/src/main.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QDir::setCurrent(QCoreApplication::applicationDirPath());
     MainWindow w;
     w.show();
     return QApplication::exec();

328a7871a9265c3cde9565202f487657b6361e79
Ivo van Poorten <ivopvp@gmail.com>
2024-10-21 21:22:05 +0200

    Better recovery from enter/exit monitor

--- src/main.c
+++ src/main.c
@@ -630,9 +630,10 @@ int main_program(int argc, char **argv) {
                     screen_hide();
                     double remember = SDL_GetTicks();
                     if (!monitor()) goto exit_out;
+                    screen_unhide();
+                    screen_update();
                     double elapsed = SDL_GetTicks() - remember;
                     target += elapsed;
-                    screen_unhide();
                     }
                     break;
                 case SDLK_F9:

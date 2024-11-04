8dafd6028ff13998918a476b7e70c2a991207242
Ivo van Poorten <ivopvp@gmail.com>
2024-11-04 20:24:12 +0100

    Start 0.9.2 cycle

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
 #pragma once
 #define VERSION_MAJOR       0
 #define VERSION_MINOR       9
-#define VERSION_SUBMINOR    1
-#define VERSION_STRING      "version 0.9.1"
+#define VERSION_SUBMINOR    2
+#define VERSION_STRING      "version 0.9.2"

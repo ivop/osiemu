4c08b5077f91e8b3f255e21315ad2b019bc11200
Ivo van Poorten <ivopvp@gmail.com>
2024-11-15 00:55:34 +0100

    Start 0.9.3 development branch

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
 #pragma once
 #define VERSION_MAJOR       0
 #define VERSION_MINOR       9
-#define VERSION_SUBMINOR    2
-#define VERSION_STRING      "version 0.9.2"
+#define VERSION_SUBMINOR    3
+#define VERSION_STRING      "version 0.9.3"
